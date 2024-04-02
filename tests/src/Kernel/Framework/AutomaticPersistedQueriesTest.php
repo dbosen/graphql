f02464f48a389070e69589f528fe50f1f15d76a4
Daniel Bosen <daniel.bosen@burda.com>
2024-04-02 14:14:26 +0200

    new approach

--- tests/src/Kernel/Framework/AutomaticPersistedQueriesTest.php
+++ tests/src/Kernel/Framework/AutomaticPersistedQueriesTest.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\Tests\graphql\Kernel\Framework;
 
-use Drupal\node\Entity\Node;
-use Drupal\node\Entity\NodeType;
 use Drupal\Tests\graphql\Kernel\GraphQLTestBase;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -18,8 +16,7 @@ class AutomaticPersistedQueriesTest extends GraphQLTestBase {
    * {@inheritdoc}
    */
   protected static $modules = [
-    'page_cache',
-    'dynamic_page_cache',
+    'page_cache'
   ];
 
   /**
@@ -41,43 +38,29 @@ class AutomaticPersistedQueriesTest extends GraphQLTestBase {
         query: Query
       }
       type Query {
-        node(id: String): Node
         field_one: String
       }
+  GQL;
 
-      type Node {
-        title: String!
-      }
-GQL;
     $this->setUpSchema($schema);
     $this->mockResolver('Query', 'field_one', 'this is the field one');
-    $this->mockResolver('Query', 'node',
-      $this->builder->produce('entity_load')
-        ->map('type', $this->builder->fromValue('node'))
-        ->map('id', $this->builder->fromArgument('id'))
-    );
-
-    $this->mockResolver('Node', 'title',
-      $this->builder->produce('entity_label')
-        ->map('entity', $this->builder->fromParent())
-    );
 
     /** @var \Drupal\graphql\Plugin\DataProducerPluginManager $manager */
     $manager = $this->container->get('plugin.manager.graphql.persisted_query');
 
     $this->pluginApq = $manager->createInstance('automatic_persisted_query');
-
-    // Before adding the persisted query plugins to the server, we want to make
-    // sure that there are no existing plugins already there.
-    $this->server->removeAllPersistedQueryInstances();
-    $this->server->addPersistedQueryInstance($this->pluginApq);
-    $this->server->save();
   }
 
   /**
    * Test the automatic persisted queries plugin.
    */
   public function testAutomaticPersistedQueries(): void {
+    // Before adding the persisted query plugins to the server, we want to make
+    // sure that there are no existing plugins already there.
+    $this->server->removeAllPersistedQueryInstances();
+    $this->server->addPersistedQueryInstance($this->pluginApq);
+    $this->server->save();
+
     $endpoint = $this->server->get('endpoint');
 
     $query = 'query { field_one } ';
@@ -124,59 +107,6 @@ GQL;
     $result = $this->container->get('http_kernel')->handle($request);
     $this->assertSame(200, $result->getStatusCode());
     $this->assertSame(['data' => ['field_one' => 'this is the field one']], json_decode($result->getContent(), TRUE));
-  }
-
-  /**
-   * Test APQ with dynamic page cache.
-   *
-   * Tests that cache context for different variables parameter is correctly
-   * added to the dynamic page cache entries.
-   */
-  public function testPageCacheWithDifferentVariables(): void {
-    $endpoint = $this->server->get('endpoint');
-
-    NodeType::create([
-      'type' => 'test',
-      'name' => 'Test',
-    ])->save();
-
-    $node = Node::create([
-      'nid' => 1,
-      'title' => 'Node 1',
-      'type' => 'test',
-    ]);
-    $node->save();
-
-    $node = Node::create([
-      'nid' => 2,
-      'title' => 'Node 2',
-      'type' => 'test',
-    ]);
-    $node->save();
-
-    // Post query to endpoint to get the result and cache it.
-    $query = 'query($id: String!) { node(id: $id) { title } }';
-    $parameters['extensions']['persistedQuery']['sha256Hash'] = hash('sha256', $query);
-    $parameters['variables'] = '{"id": "2"}';
-    $content = json_encode(['query' => $query] + $parameters);
-    $request = Request::create($endpoint, 'POST', [], [], [], ['CONTENT_TYPE' => 'application/json'], $content);
-    $result = $this->container->get('http_kernel')->handle($request);
-    $this->assertSame(200, $result->getStatusCode());
-    $this->assertSame(['data' => ['node' => ['title' => 'Node 2']]], json_decode($result->getContent(), TRUE));
-
-    // Execute apq call.
-    $parameters['variables'] = '{"id": "1"}';
-    $request = Request::create($endpoint, 'GET', $parameters);
-    $result = $this->container->get('http_kernel')->handle($request);
-    $this->assertSame(200, $result->getStatusCode());
-    $this->assertSame(['data' => ['node' => ['title' => 'Node 1']]], json_decode($result->getContent(), TRUE));
-
-    // Execute apq call with different variables.
-    $parameters['variables'] = '{"id": "2"}';
-    $request = Request::create($endpoint, 'GET', $parameters);
-    $result = $this->container->get('http_kernel')->handle($request);
-    $this->assertSame(200, $result->getStatusCode());
-    $this->assertSame(['data' => ['node' => ['title' => 'Node 2']]], json_decode($result->getContent(), TRUE));
   }
 
 }
